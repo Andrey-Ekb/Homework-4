d9941e19a0dbe2f2ac1713a29bdf17ae7946f756
Andrey <Andre.68.1@mail.ru>
2023-02-13 10:45:17 +0500

    sum of odd positions

---
+++ Homework4_004/Program.cs
@@ -0,0 +1,23 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+Console.Clear();
+Console.Write("Введите величину массива - ");
+int number=int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+int []array=new int[number];
+int resNegative=0;
+
+for (int i=0;i<number; i++)  
+{
+    array[i] = new Random().Next(1,10);
+    Console.Write(array[i]+" | ");
+}
+Console.WriteLine();
+for (int i=0;i<number; i++)
+{ 
+    if (array[i]%2>0) resNegative=resNegative+array[i];
+}
+
+Console.WriteLine();
+Console.WriteLine("Сумма нечетных чисел равна "+resNegative);
+Console.WriteLine();
+
