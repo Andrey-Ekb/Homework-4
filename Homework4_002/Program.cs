60ce2f467417860e433579454f4fba233f02fff0
Andrey <Andre.68.1@mail.ru>
2023-02-13 10:30:50 +0500

    number of even/odd numbers

---
+++ Homework4_002/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задать массив, заполнить случайными положительными трехзначными числами. 
+// Показать количество четных и нечетных чисел.()
+
+Console.Clear();
+Console.Write("Введите величину массива - ");
+int number=int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+int []array=new int[number];
+int resPositive=0;
+int kolPositive=0;
+int resNegative=0;
+int kolNegative=0;
+
+for (int i=0;i<number; i++)  
+{
+    array[i] = new Random().Next(100,1000);
+    Console.Write(array[i]+" | ");
+}
+Console.WriteLine();
+for (int i=0;i<number; i++)
+{ 
+    if (array[i]%2==0) 
+    { 
+        resPositive=resPositive+array[i];
+        kolPositive++;
+    }
+    else 
+    { 
+        resNegative=resNegative+array[i];
+        kolNegative++;
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Четных цифр в массиве "+ kolPositive+" величиной равной "+resPositive);
+Console.WriteLine("Нечетных цифр в массиве "+kolNegative+" величиной равной "+resNegative);
+Console.WriteLine();
