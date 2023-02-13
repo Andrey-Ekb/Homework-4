949a14e6ea2b64d15a0f6b9807dd0eeb7898a7cc
Andrey <Andre.68.1@mail.ru>
2023-02-13 10:22:22 +0500

    array filling

---
+++ Homework4_001/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Задать массив из восьми элементов, заполненных нулями и единицами. Вывести на экран.
+Console.Clear();
+Console.WriteLine();
+
+int[]array=new int[8];
+int i=0; 
+int length=array.Length;
+
+for (i=0; i<length; i++) array[i] = new Random().Next(0,2);
+
+for (i=0; i<length; i++) Console.Write(" "+i+" |");
+
+Console.WriteLine();  
+
+for (i=0; i<length; i++) Console.Write(" "+array[i]+" |");
+
+Console.WriteLine();
